35ca25dac0d8ff9e880f6a7b444d593998b7a468
le minh trung <you@example.com>
2021-10-26 11:37:16 +0700

    first commit

---
+++ src/basic/dev/MainAppp.java
@@ -0,0 +1,17 @@
+package basic.dev;
+
+public class MainAppp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short s = 1;
+		byte b = (byte) s;
+		float f = 3.5f;
+		double d = 4.5;
+		char c = 'y';
+		String ten = null;
+		System.out.println("Ten sinh vien" + ten);
+		
+	}
+
+}
